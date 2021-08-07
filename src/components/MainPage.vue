4a5b4fdeb0c165b299334f81fdb4a932b83462cb
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-08-07 21:04:49 +0700

    Refactor component "HelloWorld" to "MainPage". Install SASS.

---
+++ src/components/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="WeatherApp">
+    <div class="header"> {{msg}}</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  },
+  data() {
+    this.msg = 'Weather'
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  color: palevioletred;
+}
+</style>
