c8acd5528f8b4f9ad8d8eb5a1a6478b66155ad8e
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-08-07 23:40:33 +0700

    Some styles changed

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #2c1147;
+  margin: 0;
+  padding: 0;
 }
 
 </style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="weather-page">
-    <div>
-    <div class="weather-page__header">
-      <h1 class="weather-page__header-text">{{msg}}</h1>
+  <div class="page">
+    <div class="weather">
+    <div class="weather__header">
+      <h1 class="weather__header-text">{{msg}}</h1>
     </div>
-    <div class="weather-page__search">
+    <div class="weather__search">
       <el-select
-          class="weather-page__selector"
+          class="weather__selector"
           v-model="value"
           placeholder="Select">
       <el-option
@@ -16,9 +16,9 @@
           :value="item.value">
       </el-option>
     </el-select>
-      <el-button class="weather-page__button" type="primary" plain>Искать</el-button>
+      <el-button class="weather__button" type="primary" plain>Искать</el-button>
     </div>
-    <div class="weather-page__table">
+    <div class="weather__table">
      <el-table
         :data="tableData">
       <el-table-column
@@ -34,8 +34,8 @@
     </el-table>
      </div>
     </div>
-    <div class="weather-page__map">
-      <iframe height="250px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2297.267837347828!2d83.05014391544931!3d54.845545669061586!2m3!1f0!2f0!3f0!3m!1i1024!2i768!4f13.1!3m3!1m2!1s0x42dfc49ea6083ab9%3A0x4cd4ef286908078d!2z0KDRg9GB0YHQutCw0Y8g0YPQuy4sIDM1LCDQndC-0LLQvtGB0LjQsdC40YDRgdC6LCDQndC-0LLQvtGB0LjQsdC40YDRgdC60LDRjyDQvtCx0LsuLCDQoNC-0YHRgdC40Y8sIDYzMDA1OA!5e0!3m2!1sru!2sus!4v1611852565792!5m2!1sru!2sus"
+    <div class="weather__map">
+      <iframe height="500px" width="500px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2297.267837347828!2d83.05014391544931!3d54.845545669061586!2m3!1f0!2f0!3f0!3m!1i1024!2i768!4f13.1!3m3!1m2!1s0x42dfc49ea6083ab9%3A0x4cd4ef286908078d!2z0KDRg9GB0YHQutCw0Y8g0YPQuy4sIDM1LCDQndC-0LLQvtGB0LjQsdC40YDRgdC6LCDQndC-0LLQvtGB0LjQsdC40YDRgdC60LDRjyDQvtCx0LsuLCDQoNC-0YHRgdC40Y8sIDYzMDA1OA!5e0!3m2!1sru!2sus!4v1611852565792!5m2!1sru!2sus"
               style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
       </iframe>
     </div>
@@ -96,36 +96,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.weather-page {
+.page {
+  display: flex;
+  align-items: stretch;
+  justify-content: space-between;
+}
+.weather {
   display: flex;
   flex-direction: column;
-  align-items: stretch;
-  height: 100vh;
-  width: 100vw;
-  margin-top: -80px;
+  align-items: flex-start;
+
   &__header {
     color: black;
-    margin: 20px;
+    margin-left: 20px;
   }
   &__header-text {
-    box-shadow: 0 0 5px  #000000;
+    box-shadow: 0 0 5px #1dfae0;
     padding: 20px;
+    color: #ffffff ;
   }
   &__search {
     margin: 20px;
   }
   &__selector {
-    box-shadow: 0 0 5px  #000000;
+    box-shadow: 0 0 5px  #1dfae0;
   }
   &__button {
-    box-shadow: 0 0 5px  #000000;
+    box-shadow: 0 0 5px  #1dfae0;
     margin-left: 5px;
   }
   &__table {
     background-color: aquamarine;
-    box-shadow: 0 0 5px  #000000;
+    box-shadow: 0 0 5px  #1dfae0;
     margin: 20px;
     width: 720px
+  }
+  &__map {
+    height: 500px;
+    width: 500px;
+    box-shadow: 0 0 5px  #1dfae0;
+    align-items: flex-end;
+    margin-top: 103px;
+    margin-right: 20px;
   }
 }
 </style>
