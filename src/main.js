8027da08bc552794ce09ebfa08768a6df82b6fb3
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-08-08 18:28:24 +0700

    Откат обратно

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from '@/App.vue'
-import '@/styles/main.scss'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
